e53139e665d0e53dc6fc1d09226f7058a3ecd6e3
Ariel Carvalho <59920888+devMarsh4ll@users.noreply.github.com>
2021-11-12 20:48:37 -0300

    Configure api gateway

---
+++ hr-api-gateway-zulll/src/main/java/com/leomodulos/hrapigatewayzuul/HrApiGatewayZulllApplication.java
@@ -0,0 +1,17 @@
+package com.leomodulos.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class HrApiGatewayZulllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZulllApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-zulll/src/test/java/com/leomodulos/hrapigatewayzuul/HrApiGatewayZulllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leomodulos.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZulllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
