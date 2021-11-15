c90a6b7c1a2961278fcbb200938b162471fea881
Ariel Carvalho <59920888+devMarsh4ll@users.noreply.github.com>
2021-11-15 18:02:33 -0300

    Project OAuth configuration

--- hr-oauth/src/main/java/com/leomodulos/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/leomodulos/hroauth/HrOauthApplication.java
@@ -1,8 +1,10 @@
-package com.leomodulos.hruser;
+package com.leomodulos.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
 
