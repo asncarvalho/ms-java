cc91d8e6526870c0f791934018c41f64dab12b21
Ariel Carvalho <59920888+devMarsh4ll@users.noreply.github.com>
2021-11-14 23:56:12 -0300

    database seeding

--- hr-user/src/main/java/com/leomodulos/hruser/entities/Role.java
+++ hr-user/src/main/java/com/leomodulos/hruser/entities/Role.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_roles")
+@Table(name = "tb_role")
 public class Role implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
